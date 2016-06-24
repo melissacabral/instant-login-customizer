dab6dd8f9199f82f12463b7fdd8c6ec9a2f26fbb
Melissa Cabral <melissacabral@gmail.com>
2016-06-23 21:56:23 -0700

    added admin panel form
    todo: CSS output and testing!

--- form.php
+++ form.php
@@ -2,24 +2,46 @@
 	<h1>Instant Custom Login Form</h1>
 	<form action="options.php" method="post">
 		
-		<?php settings_fields( 'ilc-login-settings' );	//pass slug name of page, also referred
-		                                        //to in Settings API as option group name
-		do_settings_sections( 'ilc-login-settings' ); 	//pass slug name of page
-		submit_button();
-		?>
-
-
-
-		<!-- <?php 
+		<?php 
 		settings_fields('ilc_settings'); 
 		$values = get_option('ilc_settings' );
 		?>
-		<input type="checkbox" name="ilc_settings[use-theme]" <?php checked(1, $values['use-theme'] ); ?>>
-		<label>Use Theme's Custom Background, header and logo</label>
+		
+		<table class="form-table">
+			<tbody>
+				<tr valign="top">
+					<th scope="row">
+						<label>Theme Customizer</label>
+					</th>
+					<td>
+					<p class="description">Check these boxes if you're trying to make your login form match your theme customizations</p>
+					<fieldset>
+						<input value="1" type="checkbox" name="ilc_settings[use_background]" <?php checked(1, $values['use_background'] ); ?>>
+						<label>Use Theme's Custom Background</label>
+						<br>
+						<input value="1" type="checkbox" name="ilc_settings[use_logo]" <?php checked(1, $values['use_logo'] ); ?>>
+						<label>Use Theme's Custom Logo</label>
+						<br>
+						<input value="1" type="checkbox" name="ilc_settings[use_header]" <?php checked(1, $values['use_header'] ); ?>>
+						<label>Use Theme's Custom Header</label>
+						</fieldset>
+					</td>
+				</tr>
+				<tr valign="top">
+					<th scope="row">
+						<label>Additional CSS</label>
+					</th>
+					<td>
+						<textarea name="ilc_settings[custom_css]" class="large-text code"><?php echo $values['custom_css'] ?></textarea>
+					</td>
+				</tr>
+				
+			</tbody>
+		</table>
+		
 		<br>
-		<label>Additional Custom CSS</label>
-		<textarea name="ilc_settings[custom-css]"><?php echo $values['custom-css'] ?></textarea>
-		<?php //submit_button('Save Changes' ); ?>
-	-->
-</form>
+		
+		
+		<?php submit_button('Save Changes' ); ?>
+	</form>
 </div>
--- instant-login-customizer.php
+++ instant-login-customizer.php
@@ -8,18 +8,22 @@ Author URI: http://melissacabral.com
 Version: 0.1
 License: GPLv3
 */
+
+add_action( 'after_setup_theme', 'ilc_init' );
 function ilc_init(){
+	// Make sure necessary theme support is on
 	if (!current_theme_supports('custom-background')) {
 		add_theme_support( 'custom-background' );
 	}
 	if (!current_theme_supports('custom-logo')) {
 		add_theme_support( 'custom-logo' );
 	}
-	if (!current_theme_supports('custom-header')) {
-		add_theme_support( 'custom-header' );
-	}
+	// if (!current_theme_supports('custom-header')) {
+	// 	add_theme_support( 'custom-header' );
+	// }
 }
-add_action( 'after_setup_theme', 'ilc_init' );
+
+
 /**
  * Make sure a color has a hex # symbol at the beginning
  * @param  [type] $color [description]
@@ -37,108 +41,64 @@ function ilc_add_hash($color){
 
 
 
-function ilc_settings_api_init() {
- 	// Add the section to reading settings so we can add our
- 	// fields to it
- 	add_settings_section(
-		'ilc_setting_section',
-		'Example settings section in reading',
-		'ilc_setting_section_callback_function',
-		'ilc-login-settings'
-	);
- 	
- 	// Add the field with the names and function to use for our new
- 	// settings, put it in our new section
- 	add_settings_field(
-		'ilc_setting_name',
-		'Example setting Name',
-		'ilc_setting_callback_function',
-		'ilc-login-settings',
-		'ilc_setting_section'
-	);
- 	
- 	// Register our setting so that $_POST handling is done for us and
- 	// our callback function just has to echo the <input>
- 	register_setting( 'ilc_settings', 'ilc_setting_name' );
- } // ilc_settings_api_init()
- 
- add_action( 'admin_init', 'ilc_settings_api_init' );
- 
-  
- // ------------------------------------------------------------------
- // Settings section callback function
- // ------------------------------------------------------------------
- //
- // This function is needed if we added a new section. This function 
- // will be run at the start of our section
- //
- 
- function ilc_setting_section_callback_function() {
- 	echo '<p>Intro text for our settings section</p>';
- }
- 
- // ------------------------------------------------------------------
- // Callback function for our example setting
- // ------------------------------------------------------------------
- //
- // creates a checkbox true/false option. Other types are surely possible
- //
- 
- function ilc_setting_callback_function() {
- 	echo '<input name="ilc_setting_name" id="ilc_setting_name" type="checkbox" value="1" class="code" ' . checked( 1, get_option( 'ilc_setting_name' ), false ) . ' /> Explanation text';
- }
+/**
+ * Create the settings group for the plugin
+ * @param   
+ * @return                
+ * 
+ */
+function ilc_settings(){
+	register_setting( 'ilc_settings', 'ilc_settings', 'ilc_sanitize' );
+	//create defaults
+	add_option( 'ilc_settings', array(
+		'use_logo' => 1,
+		'use_background' => 1,
+		'use_header' => 1,
+		'custom_css' => '',
+		));
+
+}
+add_action('admin_init', 'ilc_settings' );
 
 
+/**
+ * Cleans inputs from admin panel form
+ * @param array $input raw data
+ * @return array        DB ready data
+ * @todo make this work!
+ */
+function ilc_sanitize($input){
+	$input['use_logo'] = wp_filter_nohtml_kses( $input['use_logo'] );
+	$input['use_logo'] = wp_filter_nohtml_kses( $input['use_logo'] );
+	$input['use_background'] = wp_filter_nohtml_kses( $input['use_background'] );
 
+	$input['custom_css'] = wp_filter_nohtml_kses( $input['custom_css'] );
 
-// /**
-//  * Add a textarea for more custom CSS for the login
-//  * @param   $wp_customize 
-//  * @return                
-//  * @todo Make this into a dedicated settings page, not the customizer screen (makes no sense since you can't see the login form!)
-//  */
-// function ilc_settings(){
-// 	register_setting( 'ilc_settings', 'ilc_settings', 'ilc_sanitize' );
-// 	update_option( 'ilc_settings');
-
-// }
-// add_action('admin_init', 'ilc_settings' );
-
-
-// *
-//  * Cleans inputs from admin panel form
-//  * @param array $input raw data
-//  * @return array        DB ready data
-//  * @todo make this work!
- 
-// function ilc_sanitize($input){
-	
-// 	return $input;
-// }
+	return $input;
+}
 /**
  * Admin Panel Page
  */
+add_action( 'admin_menu', 'ilc_options_page' );
 function ilc_options_page(){
 	add_theme_page( 'Login Form', 'Login Form', 'manage_options', 'ilc-login-settings', 'ilc_page_html' );
 }
-add_action( 'admin_menu', 'ilc_options_page' );
 
-function ilc_page_html(){
-	
+
+function ilc_page_html(){	
 	if(current_user_can('manage_options' )){
-
-		include(plugin_dir_path(__FILE__ ) . '/form.php');
-		
+		include(plugin_dir_path(__FILE__ ) . '/form.php');		
 	}else{
-		
+		die('You do not have permission to view this page');
 	}
 }
 
 /**
  * Embedded CSS Output
  */
-
+add_action( 'login_head', 'ilc_custom_css' );
 function ilc_custom_css(){
+	$values = get_option('ilc_settings' );
 	$bg_color = ilc_add_hash(get_background_color());
 	
 	?>
@@ -147,7 +107,9 @@ function ilc_custom_css(){
 		body, html{
 			background-color:<?php echo $bg_color; ?>;
 		}
-		<?php if(has_custom_logo()): 
+		<?php 
+		//CUSTOM LOGO
+		if(has_custom_logo() AND $values['use_logo'] ): 
 		$logo_image =  wp_get_attachment_image_url( get_theme_mod( 'custom_logo'));
 		?>
 		.login h1 a{
@@ -155,9 +117,24 @@ function ilc_custom_css(){
 			width:auto;
 			background-size:contain;
 		}
-	<?php endif; //custom logo ?>
+		<?php endif; //custom logo 
+
+		//CUSTOM HEADER
+		if(has_header_image() AND $values['use_header'] ): 
+		$header_image =  get_header_image();
+		?>
+		.login h1 {
+			background-image:url(<?php echo $header_image ?>);
+			width:auto;
+			background-size:contain;
+		}
+		<?php endif; //custom logo 
+
+	echo $values['custom_css'];
+	?>
+
+
 </style>
 <?php
 }
-add_action( 'login_head', 'ilc_custom_css' );
 
